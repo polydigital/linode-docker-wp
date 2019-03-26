6882678f2a707ecaf3c1a8465fa58e8cdb04b492
Tom Broughton <hello@tombroughton.me>
2019-03-26 18:57:52 +0000

    we can now create new WP instances

---
+++ new-wp-docker.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Get the secrets 
+while [[ -z "$SITENAME" ]]
+do
+  read -s -p "Enter a new site name for this server: " SITENAME
+done
+
+while [[ -z "$WPFQDN" ]]
+do
+  read -s -p "Enter a domain name (e.g. www.mysite.co.uk): " WPFQDN
+done
+
+while [[ -z "$CERTEMAIL" ]]
+do
+  read -s -p "Enter an email for certificate: " CERTEMAIL
+done
+
+while [[ -z "$MYSQLROOTPASSWORD" ]]
+do
+  read -s -p "Enter a password for root MySQL: " MYSQLROOTPASSWORD
+done
+
+while [[ -z "$MYSQLPASSWORD" ]]
+do
+  read -s -p "Enter a password for WP user MySQL: " MYSQLPASSWORD
+done
+
+MYSQLDBNAME = $SITENAME + "-mariadb"
+MYSQLUSER = "wp-" + $SITENAME + "-dbuser"
+WPMEMORYCONF = $SITENAME + "-memorylimit.ini"
+
+# increase allowed memory limit for uploads on wordpress - this file is mounted from home dir
+printf "file_uploads = On\nmemory_limit = 64M\nupload_max_filesize = 64M\npost_max_size = 64M\nmax_execution_time = 600" > ~/$WPMEMORYCONF
+
+docker run --name $MYSQLDBNAME --net dockerwp \
+	-v mariadb1:/var/lib/mysql \
+	-e MYSQL_ROOT_PASSWORD=$MYSQLROOTPASSWORD \
+	-e MYSQL_DATABASE=$MYSQLDBNAME \
+	-e MYSQL_USER=$MYSQLUSER \
+	-e MYSQL_PASSWORD=$MYSQLPASSWORD \
+	-d --restart always mariadb
+
+docker run --name wordpress1 --net dockerwp \
+	-v wordpress1:/var/www/html \
+	-v ~/$WPMEMORYCONF:/usr/local/etc/php/conf.d/uploads.ini \
+	-e WORDPRESS_DB_HOST=mariadb1:3306 \
+	-e WORDPRESS_DB_NAME=$MYSQLDBNAME \
+	-e WORDPRESS_DB_USER=$MYSQLUSER \
+	-e WORDPRESS_DB_PASSWORD=$MYSQLPASSWORD \
+	-e VIRTUAL_HOST=$WPFQDN \
+	-e LETSENCRYPT_HOST=$WPFQDN \
+	-e LETSENCRYPT_EMAIL=$CERTEMAIL \
+	-d --restart always wordpress
